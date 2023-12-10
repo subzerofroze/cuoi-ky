af77fd3df3f076ba6d6ecc5987dd83c34326e8d1
phamthuyquy <phamthuyquy2112@gmail.com>
2023-12-10 21:24:36 +0700

    Set 1 maingame

---
+++ My project/Assets/Char3.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char3 : MonoBehaviour
+{
+
+    public Transform startLine;
+    public Transform finishLine;
+
+    public Rigidbody2D Rchar3;
+    public BoxCollider2D Cchar3;
+
+    public float minSpeed = 50f;
+    public float maxSpeed = 250f;
+
+    public Vector2 direction = Vector2.right;
+    public bool reversed = false; // Đánh dấu hướng di chuyển đảo ngược
+    public bool movingRight = true;
+
+    public int targetCount = 2; // Số lần cần đi qua vạch đích để dừng lại
+    public int currentCount = 0; // Biến đếm số lần đã đi qua vạch đích
+
+    public bool firstStartLineCollision = true; // Đánh dấu đã đi qua vạch xuất phát lần đầu tiên
+
+    public float finishTime;
+    public bool hasFinished = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveCharacter(Rchar3);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        // Kiểm tra nếu GameObject chạm vào startLine
+        if (other.transform == startLine && !firstStartLineCollision)
+        {
+            //Debug.Log( Character3.name + "collided with the start line!");
+
+            movingRight = true;
+
+            // Đảo ngược hướng di chuyển và xoay nhân vật
+            ReverseDirection();
+        }
+        // Kiểm tra nếu GameObject chạm vào finishLine
+        else if (other.transform == finishLine)
+        {
+
+            movingRight = false;
+
+            // Tăng biến đếm số lần đi qua vạch đích
+            currentCount++;
+
+            if (currentCount < targetCount)
+            {
+                // Nếu chưa đi qua đủ số lần, đảo ngược hướng di chuyển và xoay nhân vật
+                ReverseDirection();
+            }
+        }
+
+        // Đánh dấu đã đi qua vạch xuất phát lần đầu tiên
+        if (other.transform == startLine)
+        {
+            firstStartLineCollision = false;
+        }
+    }
+
+    void ReverseDirection()
+    {
+        // Đảo ngược hướng di chuyển
+        reversed = !reversed;
+
+        // Đảo ngược hướng xoay của nhân vật
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+
+        if (movingRight)
+        {
+            direction = Vector2.right;
+        }
+        else
+        {
+            direction = Vector2.left;
+        }
+
+    }
+
+    void StopMovement(Rigidbody2D rb)
+    {
+        rb.velocity = Vector2.zero;
+    }
+
+    void MoveCharacter(Rigidbody2D rb)
+    {
+
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+
+        // Di chuyển theo hướng và tốc độ xác định
+        rb.velocity = direction * randomSpeed;
+
+    }
+
+    public string GetName()
+    {
+        // Tạo và đặt tên cho GameObject trong script
+        GameObject Character3 = new GameObject("Char3Name");
+
+        // Đặt tên cho GameObject thông qua thuộc tính name
+        Character3.name = "Kurt";
+
+        return Character3.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentCount == targetCount)
+        {
+            StopMovement(Rchar3);
+        }
+        else
+        {
+            MoveCharacter(Rchar3);
+        }
+
+        if (currentCount == targetCount && !hasFinished)
+        {
+            hasFinished = true;
+
+            finishTime = Time.time;
+
+            Debug.Log(GetName() + " collided with the finish line at " + finishTime);
+        }
+
+        OnTriggerEnter2D(Cchar3);
+
+        CurrentPosition();
+    }
+
+    public float CurrentPosition()
+    {
+        return transform.position.x;
+    }
+
+    public float FinishTime()
+    {
+        return finishTime;
+    }
+
+}
---
+++ My project/Assets/DiChuyen.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiChuyen : MonoBehaviour
+{
+    public Char1 char1;
+    public Char2 char2;
+    public Char3 char3;
+    public Char4 char4;
+    public Char5 char5;
+
+    public Text addtext;
+
+    public float[] finishTimes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        char1 = GameObject.FindGameObjectWithTag("Char1").GetComponent<Char1>();
+        char2 = GameObject.FindGameObjectWithTag("Char2").GetComponent<Char2>();
+        char3 = GameObject.FindGameObjectWithTag("Char3").GetComponent<Char3>();
+        char4 = GameObject.FindGameObjectWithTag("Char4").GetComponent<Char4>();
+        char5 = GameObject.FindGameObjectWithTag("Char5").GetComponent<Char5>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimesArray();
+
+        Finalranking();
+    }
+
+    void TimesArray()
+    {
+        // Khởi tạo mảng với 5 phần tử
+        finishTimes = new float[5];
+
+        finishTimes[0] = char1.FinishTime();
+        finishTimes[1] = char2.FinishTime();
+        finishTimes[2] = char3.FinishTime();
+        finishTimes[3] = char4.FinishTime();
+        finishTimes[4] = char5.FinishTime();
+
+        // Sắp xếp mảng tăng dần
+        System.Array.Sort(finishTimes);
+
+    }
+
+   
+    /*
+    public void CurrentRanking()
+    {
+        //Vị trí thứ nhất theo thời gian
+        if (finishTimes[0] == char1.CurrentPosition())
+        {
+            addtext.text = char1.GetName() + " is in 1st place";
+        }
+        if (finishTimes[0] == char2.CurrentPosition())
+        {
+            addtext.text = char2.GetName() + " is in 1st place";
+        }
+        if (finishTimes[0] == char3.CurrentPosition())
+        {
+            addtext.text = char3.GetName() + " is in 1st place";
+        }
+        if (finishTimes[0] == char4.CurrentPosition())
+        {
+            addtext.text = char4.GetName() + " is in 1st place  ";
+        }
+        if (finishTimes[0] == char5.CurrentPosition())
+        {
+            addtext.text = char5.GetName() + " is in 1st place";
+        }
+    }
+    */
+
+    [ContextMenu("Add Text")]
+    public void Finalranking()
+    {
+        //Vị trí thứ nhất
+        if (finishTimes[0] == char1.FinishTime())
+        {
+            addtext.text = char1.GetName() + " finished in 1st place in " + char1.FinishTime().ToString() + " s";
+        }
+        if (finishTimes[0] == char2.FinishTime())
+        {
+            addtext.text = char2.GetName() + " finished in 1st place in " + char2.FinishTime().ToString() + " s";
+        }
+        if (finishTimes[0] == char3.FinishTime())
+        {
+            addtext.text = char3.GetName() + " finished in 1st place in " + char3.FinishTime().ToString() + " s";
+        }
+        if (finishTimes[0] == char4.FinishTime())
+        {
+            addtext.text = char4.GetName() + " finished in 1st place in " + char4.FinishTime().ToString() + " s";
+        }
+        if (finishTimes[0] == char5.FinishTime())
+        {
+            addtext.text = char5.GetName() + " finished in 1st place in " + char5.FinishTime().ToString() + " s";
+        }
+
+        //Vị trí thứ hai
+        if (finishTimes[1] == char1.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char1.GetName() + " finished in 2nd place in " + char1.FinishTime().ToString() + " s");
+        }
+        if (finishTimes[1] == char2.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char2.GetName() + " finished in 2nd place in " + char2.FinishTime().ToString() + " s");
+        }
+        if (finishTimes[1] == char3.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char3.GetName() + " finished in 2nd place in " + char3.FinishTime().ToString() + " s");
+        }
+        if (finishTimes[1] == char4.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char4.GetName() + " finished in 2nd place in " + char4.FinishTime().ToString() + " s");
+        }
+        if (finishTimes[1] == char5.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char5.GetName() + " finished in 2nd place in " + char5.FinishTime().ToString() + " s");
+        }
+
+        //Vị trí thứ ba
+        if (finishTimes[2] == char1.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char1.GetName() + " finished in 3rd place in " + char1.FinishTime().ToString() + " s");
+        }
+        if (finishTimes[2] == char2.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char2.GetName() + " finished in 3rd place in " + char2.FinishTime().ToString() + " s");
+        }
+        if (finishTimes[2] == char3.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char3.GetName() + " finished in 3rd place in " + char3.FinishTime().ToString() + " s");
+        }
+        if (finishTimes[2] == char4.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char4.GetName() + " finished in 3rd place in " + char4.FinishTime().ToString() + " s");
+        }
+        if (finishTimes[2] == char5.FinishTime())
+        {
+            addtext.text = addtext.text + "\n" + (char5.GetName() + " finished in 3rd place in " + char5.FinishTime().ToString() + " s");
+        }
+    }
+
+}
