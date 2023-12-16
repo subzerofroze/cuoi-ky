1e55248d065b7085db0686b31060d614eb6bead8
subzerofroze <nguyentrisang1985@gmail.com>
2023-12-16 22:39:29 +0700

    Mute button

--- My project/Assets/AudioManagerVolume.cs
+++ My project/Assets/AudioManagerVolume.cs
@@ -4,18 +4,41 @@ using UnityEngine.UI;
 public class AudioManagerVolume : MonoBehaviour
 {
     public Slider volumeSlider;
+    public Toggle muteToggle; // Toggle for mute/unmute
     public AudioManager audioManager;
+
+    private bool isMuted = true; // Flag to track mute status
 
     private void Start()
     {
-        // Set the initial value of the slider to match the current volume
         volumeSlider.value = audioManager.GetVolume();
         volumeSlider.onValueChanged.AddListener(SetVolume);
+
+        // Add listener to the mute/unmute toggle
+        muteToggle.onValueChanged.AddListener(SetMuteStatus);
     }
 
     // Set the volume of the AudioManager based on the slider value
     public void SetVolume(float volume)
     {
-        audioManager.SetVolume(volume);
+        if (!isMuted) // Only change volume if not muted
+        {
+            audioManager.SetVolume(volume);
+        }
+    }
+
+    // Set the mute status based on the toggle value
+    public void SetMuteStatus(bool isMuted)
+    {
+        this.isMuted = isMuted;
+        if (isMuted)
+        {
+            audioManager.SetVolume(0f); // Mute by setting volume to 0
+        }
+        else
+        {
+            // Unmute by setting volume to the slider's value
+            audioManager.SetVolume(volumeSlider.value);
+        }
     }
 }
